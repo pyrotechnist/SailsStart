2faa56d185fab5e023a150cf45d3e17e7796a5d5
pyrotechnist <xulongyuan@gmail.com>
2017-07-27 14:42:24 +0200

    first commit

---
+++ api/controllers/PromotionController.js
@@ -0,0 +1,11 @@
+/**
+ * PromotionController
+ *
+ * @description :: Server-side logic for managing promotions
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Promotion.js
@@ -0,0 +1,21 @@
+/**
+ * Promotion.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+      title:{
+        type : 'string'
+      },
+      id:{
+         type: 'integer',
+         primaryKey: true,
+         autoIncrement: true
+      }
+  }
+};
+
